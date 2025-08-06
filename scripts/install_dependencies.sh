6d73e690e33465fa4bb127cfa03fb3efb97d266a
HopzAlot <rehansaqib2006@gmail.com>
2025-08-06 16:49:10 +0400

    updated scripts

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,21 +5,22 @@ echo "Updating system packages..."
 sudo yum update -y
 
 echo "Enabling python3.8 in amazon-linux-extras..."
-sudo amazon-linux-extras enable python3.8 -y
+sudo amazon-linux-extras enable python3.8
+sudo yum clean metadata
 
 echo "Installing Python 3.8 and pip..."
-sudo yum clean metadata
-sudo yum install -y python3 python3-pip
+sudo yum install -y python3.8 python3.8-venv python3.8-pip
 
-python3 --version
+echo "Checking Python version..."
+python3.8 --version
 
 # Navigate to the deployment root directory (one level up from the scripts folder)
 DEPLOY_ROOT="$(dirname "$0")/.."
 echo "Navigating to project directory at $DEPLOY_ROOT/backend"
 cd "$DEPLOY_ROOT/backend"
 
-echo "Creating virtual environment with Python 3..."
-python3 -m venv venv
+echo "Creating virtual environment with Python 3.8..."
+python3.8 -m venv venv
 source venv/bin/activate
 
 echo "Upgrading pip..."
